01b85f0014a75a84b34cb07661f766231bb70f67
Chris Owen <cowen1992@hotmail.co.uk>
2019-03-08 17:06:40 +0200

    Hide Sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import SchoolTable from "./SchoolTable";
 import SideBar from "./SideBar";
 import Grid from "@material-ui/core/Grid";
 import Footer from "./Footer";
-import Paper from "@material-ui/core/Paper";
 
 const style = {
   padding: "20px 20px 20px 20px"
@@ -17,9 +16,7 @@ const App = () => (
       <Grid item xs={9}>
         <SchoolTable />
       </Grid>
-      <Grid item xs={3}>
-        <SideBar />
-      </Grid>
+      <Grid item xs={3} />
     </Grid>
     <Footer />
   </div>

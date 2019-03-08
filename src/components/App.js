33c1b93d4fe6079bf2307187ea886c94d75b27bc
Chris Owen <cowen1992@hotmail.co.uk>
2019-03-08 15:56:56 +0200

    Rename school table file

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SimpleAppBar from "./AppBar";
-import SchoolTable from "./SchoolTableNew";
+import SchoolTable from "./SchoolTable";
 
 const App = () => (
   <div>

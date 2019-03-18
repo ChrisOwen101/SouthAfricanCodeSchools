9792e5251aaffe250eb2d8abacadbdeaa455a410
Chris Owen <cowen1992@hotmail.co.uk>
2019-03-18 16:44:02 +0200

    Add contact us in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Link } from '@material-ui/core';
+import React from "react";
+import { Link } from "@material-ui/core";
 
 const Footer = () => {
   return (
-    <div id='footer'>
-      <Link href="https://github.com/ChrisOwen101/SouthAfricanCodeSchools">Go to the source on Github</Link>
-      <Link href="https://docs.google.com/forms/d/e/1FAIpQLSciBsoj5AH6RKc0-DAh_x3QAggP6bCkCFYdCN3Tdu4rKEmJlw/viewform">Add a School</Link>
+    <div id="footer">
+      <Link href="https://github.com/ChrisOwen101/SouthAfricanCodeSchools">
+        Go to the source on Github
+      </Link>
+      <Link href="https://docs.google.com/forms/d/e/1FAIpQLSciBsoj5AH6RKc0-DAh_x3QAggP6bCkCFYdCN3Tdu4rKEmJlw/viewform">
+        Add a School
+      </Link>
+      <Link href="mailto:info@codeschooldirectory.org">
+        Something wrong? Contact us
+      </Link>
     </div>
-
   );
-}
+};
 
 export default Footer;

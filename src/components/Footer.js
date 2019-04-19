33df875660dd9fd6c27a9bc498b2a2f82175ea7c
George Ziady <george@springfisher.com>
2019-04-19 11:44:51 +0200

    Simplified footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { Link, Typography } from "@material-ui/core";
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import CodeIcon from '@material-ui/icons/Code';
+import { Link} from "@material-ui/core";
 import License from "./License";
 
 const Footer = () => {
   return (
     <div id="footer">
       <Link href="https://github.com/ChrisOwen101/SouthAfricanCodeSchools">
-        <ListItem button key="github">
-          <ListItemIcon><CodeIcon /></ListItemIcon>
-          <ListItemText primary="View the source on GitHub" />
-        </ListItem>
+        &lt; View the source on Github /&gt;
       </Link>
       <License />
     </div>

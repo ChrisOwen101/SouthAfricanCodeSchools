b90205a56186737098b82a9b91133b6d19a379a9
George Ziady <george@springfisher.com>
2019-04-28 06:22:18 +0200

    Add GA analytics package for basic recording of visitor numbers.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,19 @@ import Grid from "@material-ui/core/Grid";
 import Footer from "./Footer";
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import muiTheme from '../theme/muiTheme';
+import ReactGA from 'react-ga';
+
+if (process.env.REACT_APP_ANALYTICS_KEY) {
+  // Initalize analytics.
+  // For debugging can add: { testMode: true }
+  ReactGA.initialize(process.env.REACT_APP_ANALYTICS_KEY);
+  ReactGA.pageview('/home');
+} else {
+  // Setup dummy functions so that no errors are triggered.
+  console.log('Analytics disabled: no key.');
+  ReactGA.event = function () {};
+  ReactGA.pageview = function () {};
+}
 
 const App = () => (
   <MuiThemeProvider theme={muiTheme}>

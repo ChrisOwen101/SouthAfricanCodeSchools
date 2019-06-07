faec4796cd67387619015714100fb0f0222c99c4
George Ziady <george@springfisher.com>
2019-06-07 15:51:57 +0200

    Add button to scroll from school view to comparison table.

--- src/components/SchoolPopUp.js
+++ src/components/SchoolPopUp.js
@@ -4,12 +4,17 @@ import Paper from "@material-ui/core/Paper";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import SchoolContent from "./SchoolContent";
+import DownIcon from '@material-ui/icons/ArrowDownward';
 
 class SchoolPopUp extends Component {
   state = {};
 
   handleClose = event => {
     this.props.onClose();
+  };
+
+  handleScroll = event => {
+    document.getElementById('appTitleBar').scrollIntoView();
   };
 
   render() {
@@ -30,6 +35,16 @@ class SchoolPopUp extends Component {
         <DialogContent>
           <SchoolContent school={this.props.school} />
         </DialogContent>
+        <Button
+          variant="contained"
+          size="medium"
+          color="primary"
+          style={{textAlign:"center", width:"100%"}}
+          onClick={this.handleScroll}
+        >
+          See Other Schools
+          <DownIcon/>
+        </Button>
         <DialogActions>
           <Button onClick={this.handleClose} color="primary">
             Close
